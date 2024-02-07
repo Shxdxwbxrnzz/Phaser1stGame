cf5d42e2163254a775b0e504217a03deadf97b59
Shxdxwbxrnzz <158022767+Shxdxwbxrnzz@users.noreply.github.com>
2024-02-07 12:09:07 +0200

    Ds

--- game.js
+++ game.js
@@ -3,6 +3,13 @@ var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
     scene: {
         preload: preload,
         create: create,
@@ -12,7 +19,7 @@ var config = {
 
 
 var game = new Phaser.Game(config);
-//Добавляємо спрайти
+//Завантажуємо спрайти
 function preload ()
 {
 this.load.image('sky', 'assets/sky.png');
@@ -24,10 +31,16 @@ this.load.spritesheet('dude',
     {frameWidth: 32, frameHeight: 48}
 );
 }
-
+//Додаємо спрайти до сцени
 function create ()
 {
 this.add.image(400, 300, 'sky');
+platforms = this.physics.add.staticGroup();
+platforms.create(400, 590, 'ground').setScale(1.2).refreshBody();
+platforms.create(600, 400, 'ground').setScale(0.5).refreshBody();
+platforms.create(800, 400, 'ground').setScale(0.5).refreshBody();
+platforms.create(50, 250, 'ground').setScale(0.5).refreshBody();
+platforms.create(750, 220, 'ground').setScale(0.5).refreshBody();
 }
 
 function update ()
